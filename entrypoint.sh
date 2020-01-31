bd662e5e8482119aae50f593563000ae6270124f
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-31 08:48:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -eu
 
-curl -X POST -H "Content-Type: application/json" --data "{ \"data\": \"$data\" }" $WEBHOOK_URL
+RESPONSE=$(curl -S -X POST -H "Content-Type: application/json" --data "{ \"commit\": \"${GITHUB_SHA}\", \"ref\": \"${GITHUB_REF}\", \"default_branch\": \"master\" }" ${WEBHOOK_URL})
+
+if [[ ${RESPONSE} == "Okay" ]]; then
+    echo "exit 0, ${RESPONSE}"
+    exit 0
+else
+    echo "exit 1, ${RESPONSE}"
+    exit 1
+fi
